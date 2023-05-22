a4cc3503aa0b4ed63751fcd5ce4ecd45d9a519cc
StarryZhang-whu <93083801+StarryZhang-whu@users.noreply.github.com>
2023-05-22 16:29:17 +0800

    Initial Commit

---
+++ SportRecord/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SportRecord
+//
+//  Created by 张凌浩 on 2023/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SportRecord/SportRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportRecordApp.swift
+//  SportRecord
+//
+//  Created by 张凌浩 on 2023/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SportRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
